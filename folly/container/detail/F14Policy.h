597c6990a8b8d1f64ebe8b6de4a38f9d08c27f9c
Yeolar <yeolar@qq.com>
2020-07-26 23:56:30 +0800

    fix compile on gcc-8.3

--- folly/container/detail/F14Policy.h
+++ folly/container/detail/F14Policy.h
@@ -1148,7 +1148,10 @@ class VectorContainerPolicy : public BasePolicy<
     complainUnlessNothrowMove<lift_unit_t<MappedTypeOrVoid>>();
 
     if (valueIsTriviallyCopyable()) {
-      std::memcpy(dst, src, n * sizeof(Value));
+      std::memcpy(
+          static_cast<void*>(dst),
+          static_cast<void const*>(src),
+          n * sizeof(Value));
     } else {
       for (std::size_t i = 0; i < n; ++i, ++src, ++dst) {
         // TODO(T31574848): clean up assume-s used to optimize placement new
@@ -1173,7 +1176,10 @@ class VectorContainerPolicy : public BasePolicy<
     Value* dst = std::addressof(values_[0]);
 
     if (valueIsTriviallyCopyable()) {
-      std::memcpy(dst, src, size * sizeof(Value));
+      std::memcpy(
+          static_cast<void*>(dst),
+          static_cast<void const*>(src),
+          size * sizeof(Value));
     } else {
       for (std::size_t i = 0; i < size; ++i, ++src, ++dst) {
         try {
